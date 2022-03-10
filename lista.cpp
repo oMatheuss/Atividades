9388bac81002b60eef5c83d74f96f812f143fcb2
matheus <86634357+oMatheuss@users.noreply.github.com>
2022-03-10 19:14:56 -0300

    Add files via upload

--- lista.cpp
+++ lista.cpp
@@ -55,56 +55,70 @@ void find_number(LISTA * list) {
 }
 
 int find_number_bool(LISTA * list, int n) {
-	while (true) {
+	 if (list == NULL) return 0;
+	 do {
 		if (list->num == n) return 1;
-		if (list->next == NULL) return 0;
 		list = list->next;
-	}
-}
-
-LISTA * insert_end_rec (LISTA * list, LISTA * to_add) {
-	if (list->next == NULL)
-		list->next = to_add;
-	else
-		list->next = insert_end_rec(list->next, to_add);
-	
-	return list;
+	} while (list != NULL);
+	return 0;
 }
 
 LISTA *
 insert_end (LISTA * list) {
-	LISTA * newList = new LISTA();
+	LISTA * newList = new LISTA;
+	LISTA * aux = list;
 	printf ("Informe o numero a ser adicionado:");
 	scanf ("%d", &newList->num);
 	newList->next = NULL;
-	if (list == NULL)
-		return newList;
-	return insert_end_rec(list, newList);
-}
-
-LISTA * remove_num_rec(LISTA * list, int num) {
-	if (list == NULL) {
-		return list;
-	} else if (list->num == num) {
-		return list->next;
-	}
-	list->next = remove_num_rec(list->next, num);
+	if (list == NULL) return newList;
+	while (aux->next != NULL)
+		aux = aux->next;
+	aux->next = newList;
 	return list;
 }
 
 LISTA * remove_num (LISTA * list) {
-	int t;
 	if (list == NULL) {
 		printf("lista vazia");
 		return list;
 	}
+	int t;
 	printf("Informe o numero que deseja remover: ");
 	scanf("%d", &t);
 	if (find_number_bool(list, t) == 0) {
 		printf("o numero nao esta na lista");
+		getch();
 		return list;
 	}
-	return remove_num_rec(list, t);
+	if(list->num == t) return list->next;
+	LISTA * aux = list;
+	while (aux->next->num != t) aux = aux->next;
+	aux->next = aux->next->next;
+	return list;
+}
+
+LISTA * replace(LISTA * list) {
+	LISTA * aux = list;
+	if (list == NULL) {
+		printf("lista vazia");
+		return list;
+	}
+	int num;
+	printf("Informe o numero que deseja substituir: ");
+	scanf("%d", &num);
+	while (aux != NULL) {
+		if (aux->num == num) {
+			printf("Informe o novo numero: ");
+			int newNum;
+			scanf("%d", &newNum);
+			aux->num = newNum;
+			return list;
+		}
+		aux = aux->next;
+	}
+	printf("o numero nao esta na lista");
+	getch();
+	return list;
 }
 
 void compare(LISTA * list, LISTA * list2) {
@@ -114,7 +128,7 @@ void compare(LISTA * list, LISTA * list2) {
 			getch();
 			return;
 		}
-		if (list == NULL || list2 == NULL && !(list == NULL && list2 == NULL)) {
+		if ((list == NULL || list2 == NULL) && !(list == NULL && list2 == NULL)) {
 			printf("As listas sao diferentes!");
 			getch();
 			return;
@@ -147,7 +161,7 @@ void size(LISTA * list) {
 	getch();
 }
 
-int lenght(LISTA * list) {
+int length(LISTA * list) {
 	int count = 0;
 	if (list == NULL) {
 		return 0;
@@ -162,17 +176,66 @@ int lenght(LISTA * list) {
 }
 
 int* toVector(LISTA * list) {
-	int size = lenght(list), i = 0;
-	int vec[size];
-	for(i; i < size; i++) {
+	int size = length(list), i;
+	int* vec = new int[size];
+	for(i = 0; i < size; i++) {
 		vec[i] = list->num;
 		list = list->next;
 	}
 	return vec;
 }
 
-void printVector() {
+void printVector(int* vec, int len) {
+	printf("[");
+	int i;
+	for (i = 0; i < (len - 1); i++) {
+		printf("%d, ", vec[i]);
+	}
+	printf("%d]", vec[++i]);
+	getch();
+}
+
+LISTA * concatena(LISTA * list, LISTA * list2) {
+	LISTA * aux = list;
+	while(aux->next != NULL) {
+		aux = aux->next;
+	}
+	aux->next = list2;
+	return list;
+}
+
+LISTA * copia(LISTA * list) {
+	if(list == NULL) return NULL;
 	
+	LISTA * newList = new LISTA();
+	LISTA * aux = newList;
+	
+	while(list->next != NULL) {
+		aux->num = list->num;
+		aux->next = new LISTA();
+		aux = aux->next;
+		list = list->next;
+	}
+	aux->num = list->num;
+	aux->next = NULL;
+	return newList;
+}
+
+LISTA * separa(LISTA * list, int n) {
+	if (list == NULL) return NULL;
+	while(list->num != n) {
+		list = list->next;
+		if (list == NULL) return NULL;
+	}
+	
+	return copia(list);
+}
+
+LISTA * remove_prefixo(LISTA * list, int n) {
+	if (n > length(list)) return NULL;
+	int i;
+	for (i = 0; i < n; i++) list = list->next;
+	return copia(list);
 }
 
 int main () {
@@ -201,10 +264,10 @@ int main () {
 		printf("\n");
 		
 		printf("11 -> comparar listas\n");
+		printf("12 -> concatena lista 2 na 1\n");
+		printf("13 -> separar lista da lista 1 nas 2\n");
+		printf("15 -> lista 1 para vetor\n");
 		
-		printf("\n Listas para vetor: ");
-		int* vet = toVector(list);
-		int* vet2 = toVector(list2);
 		scanf("%d", &op);
 		switch (op) {
 			case 1:
@@ -240,6 +303,27 @@ int main () {
 			case 11:
 				compare(list, list2);
 				break;
+			case 12:
+				list = concatena(list, list2);
+				list2 = NULL;
+				break;
+			case 13:
+				printf("Informe a onde deseja separar:");
+				int n;
+				scanf("%d", &n);
+				list2 = separa(list, n);
+				break;
+			case 14:
+				printf("Informe o numero de numeros que deseja remover: ");
+				int n1;
+				scanf("%d", &n1);
+				list = remove_prefixo(list, n1);
+				break;
+			case 15:
+				int* vec;
+				vec = toVector(list);
+				printVector(vec, length(list));
+				break;
 			default:
 				break;
 		}
@@ -247,4 +331,3 @@ int main () {
 	}
 	return 0;
 }
-
